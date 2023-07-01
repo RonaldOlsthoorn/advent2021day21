fbf1d7bd0864cfc32b97645ba7537c5293bb05ac
Ronald Olsthoorn <ronaldolsthoorn1@gmail.com>
2023-07-01 11:55:06 +0200

    Part I working

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,26 @@
-use std::{io::{BufReader, BufRead}, fs::File};
+use std::{io::{BufReader, BufRead}, fs::File, collections::{VecDeque}};
 
-struct Die {
-    _state: Box<dyn Iterator<Item = u8>>,
-    _counter: usize
-}
 
-impl Die {
-
-    const ALL_STATES: [u8; 100] = [
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-        11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-        21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
-        31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
-        41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
-        51, 52, 53, 54, 55, 56, 57, 58, 59, 60,
-        61, 62, 63, 64, 65, 66, 67, 68, 69, 70,
-        71, 72, 73, 74, 75, 76, 77, 78, 79, 80,
-        81, 82, 83, 84, 85, 86, 87, 88, 89, 90,
-        91, 92, 93, 94, 95, 96, 97, 98, 99, 100];
-
-    fn new() -> Self {
-        Die { _state: Box::new(Self::ALL_STATES.into_iter().cycle()), _counter: 0 }
-    }
-
-    fn roll(&mut self) -> u8 {
-        self._counter += 1;
-        self._state.next().unwrap()
-    }
-}
+#[derive(Clone)]
 struct Pawn {
-    _state: Box<dyn Iterator<Item = u8>>,
+    _state: u8,
 }
 
 impl Pawn {
 
-    const ALL_STATES: [u8; 10] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const STATES: u8 = 10;
 
     fn new(init_state: u8) -> Self {
-        let mut new_pawn = Pawn { _state: Box::new(Self::ALL_STATES.into_iter().cycle()) };
-        new_pawn.tick(init_state.into());
-        new_pawn
+        Self { _state: init_state }
     }
 
-    fn tick(&mut self, steps: usize) -> u8 {
-        (0..steps - 1).for_each(|_| {self._state.next();});
-        self._state.next().unwrap()
+    fn tick(&mut self, steps: u8) -> u8 {
+        self._state = ((self._state + steps - 1) % Self::STATES) + 1;
+        self._state
     }
 }
 
+#[derive(Clone)]
 struct Player {
     pawn: Pawn,
     score: u16
@@ -59,41 +32,98 @@ impl Player {
         Player { pawn: Pawn::new(init_state), score: 0 }
     }
 
-    fn play_turn(&mut self, die: &mut Die) {
-        let rolls = die.roll() as usize + die.roll() as usize + die.roll() as usize;
-        self.score += self.pawn.tick(rolls) as u16;
+    fn play_turn(&mut self, die_outcome: u8) {
+        self.score += self.pawn.tick(die_outcome) as u16;
     }
 }
 
-fn main() {
+#[derive(Clone)]
+struct WalkState {
+    player1: Player,
+    player2: Player,
+    multiverses: u64,
+    turn_p1: bool
+}
 
-    let lines: Vec<String> = BufReader::new(File::open("input.txt").unwrap()).lines().map(|l| l.unwrap()).collect();
+struct SchrodingerSim {
+}
+
+impl SchrodingerSim {
+
+    const DIE_FREQUENCIES: [(u8, u64);7] = [
+        (3, 1),
+        (4, 3),
+        (5, 6),
+        (6, 7),
+        (7, 6),
+        (8, 3),
+        (9, 1)
+    ];
+
+    fn simulate_schrodinger(&self, player1: &Player, player2: &Player) -> (u64, u64) {
+
+        let mut res = (0, 0);
     
-    let mut player1 = Player::new(lines[0].chars().last().unwrap().to_digit(10).unwrap() as u8);
-    let mut player2 = Player::new(lines[1].chars().last().unwrap().to_digit(10).unwrap() as u8);
+        let mut stack: VecDeque<WalkState> = VecDeque::new();
+        stack.push_back(WalkState { player1: player1.clone(), player2: player2.clone(), multiverses: 1, turn_p1: true });
+    
+        while let Some(current_state) = stack.pop_back() {
 
-    let mut die = Die::new();
-
-    let res: usize;
-
-    loop {
-        player1.play_turn(&mut die);
-
-        if player1.score >= 1000 {
-
-            res = player2.score as usize * die._counter;
-            break;
-        }
-
-        player2.play_turn(&mut die);
-
-        if player2.score >= 1000 {
-
-            res = player1.score as usize  * die._counter;
-            break;
-        }
+            if !current_state.turn_p1 && current_state.player1.score >= 21 {
+                // turn p2 but p1 already reached goal
+                res.0 += current_state.multiverses;
+            } else if current_state.turn_p1 && current_state.player2.score >= 21 {
+                // turn p1 but p2 already reached goal
+                res.1 += current_state.multiverses;
+            } else if current_state.turn_p1 && current_state.player1.score >= 20 {
+                // turn p1 and p1 cannot lose
+                res.0 += current_state.multiverses * 27;
+            } else if !current_state.turn_p1 && current_state.player2.score >= 20 {
+                // turn p2 and p2 cannot lose
+                res.1 += current_state.multiverses * 27;
+            } else {
+                self.branch_out(&current_state).into_iter().for_each(|s| stack.push_back(s));
+            }
+        }    
+        res
     }
 
-    println!("res: {}", res);
+    fn branch_out(&self, current_state: &WalkState) -> Vec<WalkState> {
+
+        let mut branches = Vec::new();
+
+        for (die_outcome, die_frequency) in Self::DIE_FREQUENCIES {
+            let mut branch = current_state.clone();
+
+            if current_state.turn_p1 {
+                branch.player1.play_turn(die_outcome);
+
+            } else {
+                branch.player2.play_turn(die_outcome);
+            }
+
+            branch.multiverses *= die_frequency;
+            branch.turn_p1 = !branch.turn_p1;
+
+            branches.push(branch);
+        }
+        
+        branches
+    }
+
+}
+
+
+fn main() {
+
+    let lines: Vec<String> = BufReader::new(File::open("test-input.txt").unwrap()).lines().map(|l| l.unwrap()).collect();
     
+    let player1 = Player::new(lines[0].chars().last().unwrap().to_digit(10).unwrap() as u8);
+    let player2 = Player::new(lines[1].chars().last().unwrap().to_digit(10).unwrap() as u8);
+
+    let sim = SchrodingerSim{};
+    let (score_player1, score_player2) = sim.simulate_schrodinger(&player1, &player2);
+
+    println!("scores p1 {} p2 {} winner: {}", score_player1, score_player2, std::cmp::max(score_player1, score_player2));
+
 }
